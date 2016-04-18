c76266521372c4d33aeb466034fdc62a55b604ac
xiaokai <kexiaokai@gmail.com>
2016-04-18 23:02:17 +0800

    [UPD] koa-es6-server

---
+++ server/koa-es6-server.js
@@ -0,0 +1,40 @@
+import _ from 'lodash'
+import fs from 'fs'
+import path from 'path'
+// koa
+import koa from 'koa'
+import serve from 'koa-static'
+import route from 'koa-route'
+import koaJson from 'koa-json'
+// other
+import coParse from 'co-busboy'
+
+var app = koa()
+const PORT = process.env.PORT || 3002
+
+var upload = function * (next) {
+  // multipart upload
+  var parts = coParse(this)
+  var part
+  
+  while (part = yield parts) {
+    var url = '/upload/' + part.filename
+    var stream = fs.createWriteStream(path.join(__dirname, '../public', url))
+    part.pipe(stream)
+    console.log('uploading %s -> %s', part.filename, stream.path)
+  }
+
+  this.body = {
+    status: 'success',
+    url: url
+  }
+}
+
+// or use absolute paths
+app.use(koaJson())
+app.use(serve(path.join(__dirname, '../public/')))
+app.use(route.post('/upload', upload))
+
+
+app.listen(PORT)
+console.log('listening on port ', PORT)
