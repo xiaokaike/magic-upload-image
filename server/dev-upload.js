5b45840cc93e8179ff2abd0c2ee48d61276d48a2
xiaokai <kexiaokai@gmail.com>
2016-04-19 00:02:12 +0800

    [ADD] add dev-upload server

---
+++ server/dev-upload.js
@@ -0,0 +1,38 @@
+"use strict"
+
+var fs = require('fs')
+var express = require('express')
+var path = require('path')
+var router = express.Router()
+var config = require('../config')
+
+var uploadPath = path.posix.join(
+  config.build.assetsPublicPath, 
+  config.build.assetsSubDirectory,
+  config.build.uploadDirectory)
+
+router.post('/upload', function(req, res) {
+  if (req.busboy) {
+    var fName = ''
+    req.busboy.on('file', (fieldname, file, fileName, encoding, mimeType) => {
+      fName = fileName
+      var stream = fs.createWriteStream(path.join(__dirname, '../', uploadPath, fileName))
+      file.pipe(stream)
+      console.log('uploading %s -> %s', fileName, stream.path)
+
+    }).on('finish', function() {
+      res.send({
+        name: 'xxx',
+        url: path.join(uploadPath, fName)
+      })
+    })
+
+    req.pipe(req.busboy)
+  } else {
+    console.log('uploadFile - busboy undefined.')
+    res.status(502)
+  }
+})
+
+
+module.exports = router
